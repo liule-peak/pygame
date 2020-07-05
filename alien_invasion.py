a0e26310bfccc6ef8cf053fa0df2281e20509cef
liule-peak <58068663+liule-peak@users.noreply.github.com>
2020-07-05 21:56:26 +0800

    hehe

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,15 @@
 import sys
 import pygame
 
+from settings im
+
 def run_game():
     pygame.init()
     screen = pygame.display.set_mode((1200,800))
     pygame.display.set_caption("Alien Invasion")
 
     bg_color = (230,230,230)
-    
+
     while True:
         for event in pygame.event.grt():
             if event.type == pygame.QUIT:
